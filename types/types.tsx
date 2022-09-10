ca5573681f5d74da3908e72ddc0f86b459549f42
welove-t <tlove.tky@gmail.com>
2022-09-10 11:12:35 +0900

    🏷️ 型追加(NotionAPIによる)

--- types/types.tsx
+++ types/types.tsx
@@ -1,5 +1,6 @@
 import { ParsedUrlQuery } from "querystring";
 import { ReactNode } from "react";
+import { BlockType } from "notion-block-renderer";
 
 export type LayoutProps = {
   children: ReactNode;
@@ -16,10 +17,15 @@ export type PageProps = {
 };
 
 export type CardProps = { page: PageType };
-export type ArticleProps = CardProps;
+export type ArticleProps = {
+  page: PageType;
+  blocks: BlockType[];
+};
 export type ArticleMetaProps = CardProps;
 
 export type IndexProps = { pages: PageType[] };
+
+export type BlockProps = { block: BlockType };
 
 export type Params = ParsedUrlQuery & {
   slug: string;
@@ -60,3 +66,10 @@ export type PageType = {
   // properties: Record<string, any>;
   properties: PropertyType;
 };
+
+// export type BlockType = {
+//   type: string;
+//   heading_1: { rich_text: RichTextType[] };
+//   heading_2: { rich_text: RichTextType[] };
+//   paragraph: { rich_text: RichTextType[] };
+// };
