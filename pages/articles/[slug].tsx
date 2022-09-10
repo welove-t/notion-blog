504a6dc27c7bc4748459d5612f7999900b6b3306
welove-t <tlove.tky@gmail.com>
2022-09-10 11:11:11 +0900

    👽️ notionAPIによる詳細情報の取得処理追加

--- pages/articles/[slug].tsx
+++ pages/articles/[slug].tsx
@@ -1,24 +1,53 @@
-import { GetServerSideProps, NextPage } from "next";
+import {
+  GetServerSideProps,
+  GetStaticPaths,
+  GetStaticProps,
+  NextPage,
+} from "next";
 import Image from "next/image";
 import React from "react";
 import ArticleMeta from "../../components/ArticleMeta";
+import Block from "../../components/Block";
 import Layout from "../../components/Layout";
 import { ArticleProps, Params } from "../../types/types";
+import { fetchBlocksByPageId, fetchPages } from "../../utils/notion";
+import { getText } from "../../utils/property";
 import { sampleCards } from "../../utils/sample";
+import NotionBlocks from "notion-block-renderer";
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
+export const getStaticPaths: GetStaticPaths = async () => {
+  const { results } = await fetchPages({});
+  const paths = results.map((page: any) => {
+    return {
+      params: {
+        slug: getText(page.properties.slug.rich_text),
+      },
+    };
+  });
+  return {
+    paths: paths,
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
   const { slug } = ctx.params as Params;
 
-  const page = sampleCards.find((data) => data.slug === slug);
+  const { results } = await fetchPages({ slug: slug });
+  const page = results[0];
+  const pageId = page.id;
+  const { results: blocks } = await fetchBlocksByPageId(pageId);
 
   return {
     props: {
       page: page,
+      blocks: blocks,
     },
+    revalidate: 10,
   };
 };
 
-const Article: NextPage<ArticleProps> = ({ page }) => {
+const Article: NextPage<ArticleProps> = ({ page, blocks }) => {
   return (
     <Layout>
       <article className="w-full">
@@ -28,7 +57,14 @@ const Article: NextPage<ArticleProps> = ({ page }) => {
         </div>
 
         {/* article */}
-        <div className="my-12">article {page.content}</div>
+        {/* <div className="my-12">
+          {blocks.map((block, index) => (
+            <Block key={index} block={block} />
+          ))}
+        </div> */}
+        <div className="my-12">
+          <NotionBlocks blocks={blocks} isCodeHighlighter={true} />
+        </div>
       </article>
     </Layout>
   );
